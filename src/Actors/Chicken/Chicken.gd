73be7362d655a03fcba7c0b931d9cf4512351ecd
Stephen Young <stephen.young.dev@gmail.com>
2020-02-16 17:08:39 -0600

    basic implementation of boids rule 2

--- src/Actors/Chicken/Chicken.gd
+++ src/Actors/Chicken/Chicken.gd
@@ -1,5 +1,7 @@
 extends "res://src/Actors/Actor.gd"
 
+export(float) var avoid_distance: = 100.0
+
 var _flock: Array = []
 
 
@@ -15,7 +17,9 @@ func _on_FlockView_body_exited(body: PhysicsBody2D):
 func _physics_process(delta):
 	var center: = get_flock_center()
 	if center != Vector2.INF:
-		move_and_slide(get_direction(center) * speed)
+		var avoid_dir = get_avoid_direction()
+		var center_dir = get_direction(center)
+		move_and_slide((avoid_dir + center_dir) * speed)
 
 
 func get_direction(target: Vector2) -> Vector2:
@@ -30,3 +34,14 @@ func get_flock_center() -> Vector2:
 			total += f.global_position
 		center = total / _flock.size()
 	return center
+
+
+func get_avoid_direction() -> Vector2:
+	var avoid_vector: = Vector2.ZERO
+	
+	for f in _flock:
+		var neighbor_pos: Vector2 = f.global_position
+		if global_position.distance_to(neighbor_pos) < avoid_distance:
+			avoid_vector += global_position.direction_to(neighbor_pos) * -1
+	
+	return avoid_vector
