b4a1eb41e8a16ddb6ecb18b5c2afbd5dc77d087e
Stephen Young <stephen.young.dev@gmail.com>
2020-02-18 08:18:33 -0600

    separate forces per vector. set view radius from parent. add dispersal button

--- src/Actors/Chicken/Chicken.gd
+++ src/Actors/Chicken/Chicken.gd
@@ -1,15 +1,26 @@
-extends "res://src/Actors/Actor.gd"
+extends KinematicBody2D
 
+export var max_speed: = 200.0
+export var mouse_follow_force: = 0.05
+export var cohesion_force: = 0.05
+export var algin_force: = 0.05
+export var separation_force: = 0.05
+export(float) var view_distance: = 50.0
 export(float) var avoid_distance: = 20.0
 
+
+var _width = ProjectSettings.get_setting("display/window/size/width")
+var _height = ProjectSettings.get_setting("display/window/size/height")
+
 var _flock: Array = []
-var _mouse_target = Vector2.INF
-var _velocity = Vector2()
+var _mouse_target: Vector2
+var _velocity: Vector2
 
 
 func _ready():
 	randomize()
-	_velocity = Vector2(rand_range(-1, 1), rand_range(-1, 1)).normalized() * speed
+	_velocity = Vector2(rand_range(-1, 1), rand_range(-1, 1)).normalized() * max_speed
+	_mouse_target = get_random_target()
 
 
 func _on_FlockView_body_entered(body: PhysicsBody2D):
@@ -23,25 +34,28 @@ func _on_FlockView_body_exited(body: PhysicsBody2D):
 
 func _input(event):
 	if event is InputEventMouseButton:
-		_mouse_target = event.position
+		if event.get_button_index() == BUTTON_LEFT:
+			_mouse_target = event.position
+		elif event.get_button_index() == BUTTON_RIGHT:
+			_mouse_target = get_random_target()
 
 
 func _physics_process(_delta):
 	var mouse_vector = Vector2.ZERO
 	if _mouse_target != Vector2.INF:
-		mouse_vector = global_position.direction_to(_mouse_target) * speed * max_force
+		mouse_vector = global_position.direction_to(_mouse_target) * max_speed * mouse_follow_force
 	
-	# get vectors to center, avoid, and alignment to flock
+	# get cohesion, alginment, and separation vectors
 	var vectors = get_flock_status(_flock)
 	
 	# steer towards vectors
-	var center_vector = vectors[0] * max_force
-	var align_vector = vectors[1] * max_force
-	var avoid_vector = vectors[2] * max_force
+	var cohesion_vector = vectors[0] * cohesion_force
+	var align_vector = vectors[1] * algin_force
+	var separation_vector = vectors[2] * separation_force
 
-	var acceleration = align_vector + avoid_vector + center_vector + mouse_vector
+	var acceleration = cohesion_vector + align_vector + separation_vector + mouse_vector
 	
-	_velocity = (_velocity + acceleration).clamped(speed)
+	_velocity = (_velocity + acceleration).clamped(max_speed)
 	
 	_velocity = move_and_slide(_velocity)
 
@@ -60,7 +74,7 @@ func get_flock_status(flock: Array):
 
 		var d = global_position.distance_to(neighbor_pos)
 		if d > 0 and d < avoid_distance:
-			avoid_vector -= (neighbor_pos - global_position).normalized() * (avoid_distance / d * speed)
+			avoid_vector -= (neighbor_pos - global_position).normalized() * (avoid_distance / d * max_speed)
 	
 	var flock_size = flock.size()
 	if flock_size:
@@ -68,7 +82,12 @@ func get_flock_status(flock: Array):
 		flock_center /= flock_size
 
 		var center_dir = global_position.direction_to(flock_center)
-		var center_speed = speed * (global_position.distance_to(flock_center) / $FlockView/ViewRadius.shape.radius)
+		var center_speed = max_speed * (global_position.distance_to(flock_center) / $FlockView/ViewRadius.shape.radius)
 		center_vector = center_dir * center_speed
 
 	return [center_vector, align_vector, avoid_vector]
+
+
+func get_random_target():
+	randomize()
+	return Vector2(rand_range(0, _width), rand_range(0, _height))
